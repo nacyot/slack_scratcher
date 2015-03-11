71ab3aeaf5094f6019b10951c828b83d2d7a49c0
nacyot <propellerheaven@gmail.com>
2015-03-11 13:56:50 +0900

    Implement file loader

--- lib/slack_scratcher/loader/file.rb
+++ lib/slack_scratcher/loader/file.rb
@@ -4,23 +4,31 @@ module SlackScratcher
   module Loader
     class File
       include Enumerable
-      
-      def initialize(target)
-        @target = target
-        @users = SlackScratcher::Model::Users.new(target)
+
+      def initialize(target_dir)
+        raise ArgumentError unless ::File.directory?(target_dir)
+
+        @target = target_dir
+        @users = load_users
       end
 
       def each
         all_files.each do |file|
-          chats = parse_log_file(file), @users
+          chats = parse_log_file(file)
           yield chats
         end
       end
-      
+
       private
+
+      def load_users
+        users = Oj.load(::File.read("#@{target}/users.json"))
+        users.map { |data| { data['id'] => data } }.inject({}, :merge)
+      end
+
       def parse_log_file(log_file)
-        logs = Oj.load(File.read(log_file))
-        SlackScratcher::Model::Chats.new logs
+          logs = Oj.load(::File.read(log_file))
+        SlackScratcher::Model::Chats.new logs, @users
       end
 
       def all_files
@@ -28,7 +36,7 @@ module SlackScratcher
           arr + list_log_files(channel)
         end
       end
-      
+
       def list_channels
         Dir["#{@target}/*/"]
       end
--- spec/slack_scratcher/loader/file_spec.rb
+++ spec/slack_scratcher/loader/file_spec.rb
@@ -1,7 +1,57 @@
 require 'spec_helper'
 
 describe SlackScratcher::Loader::File do
-  it '' do
-    
+  before :each do
+    allow(File).to receive(:read) do
+      '[{"id": "GEHJ8EK18","name": "nacyot"}]'
+    end
+  end
+
+  describe 'Initialize object' do
+    it 'requires dir argument - fail example' do
+      target = '3279#@%y.,oud'
+      expect do
+        SlackScratcher::Loader::File.new(target)
+      end.to raise_error(ArgumentError)
+    end
+
+    it 'requires dir argument - success example' do
+      target = '/'
+      expect do
+        SlackScratcher::Loader::File.new(target)
+      end.to_not raise_error
+    end
+  end
+
+  describe 'File is Enumerable' do
+    let(:file) { SlackScratcher::Loader::File.new('/') }
+
+    before :each do
+      allow(file).to receive(:all_files) do
+        ["/general/2015-03-15.json", "/general/2015-03-16.json"]
+      end
+
+      allow(file).to receive(:parse_log_file) do
+        {
+          user: 'U03O',
+          type: 'message',
+          subtype: 'channel_join',
+          text: '<@U03BRBH8N|nacyot> has joined the channel',
+          ts: '1421302869.000002'
+        }
+      end
+
+      allow(file).to receive(:load_users) do
+        { '1' => { name: 'name1' }, '2' => { 'name' => 'name2' } }
+      end
+    end
+
+    it 'respond to each and map' do
+      expect(file).to respond_to(:each, :map)
+    end
+
+    it 'can transform logs' do
+      expect(file.map { |log| log[:user] }).to eq(%w(U03O U03O))
+    end
   end
 end
