7b9112583890c4dca61872586308556a573cd0d5
nacyot <propellerheaven@gmail.com>
2015-03-12 15:00:29 +0900

    Add user_not_found_error

---
+++ lib/slack_scratcher/error.rb
@@ -0,0 +1,5 @@
+module SlackScratcher
+  module Error
+    autoload :UserNotFoundError, 'slack_scratcher/error/user_not_found_error'
+  end
+end
---
+++ lib/slack_scratcher/error/user_not_found_error.rb
@@ -0,0 +1,6 @@
+module SlackScratcher
+  module Error
+    class UserNotFoundError < StandardError
+    end
+  end
+end
