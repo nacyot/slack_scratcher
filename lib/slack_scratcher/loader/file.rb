d52af93836a84fdd4a0bd0d6aaec2de77f63368c
nacyot <propellerheaven@gmail.com>
2015-03-12 14:59:46 +0900

    Some style update in loader/file

--- lib/slack_scratcher/loader/file.rb
+++ lib/slack_scratcher/loader/file.rb
@@ -6,7 +6,7 @@ module SlackScratcher
       include Enumerable
 
       def initialize(target_dir)
-        raise ArgumentError unless ::File.directory?(target_dir)
+        fail ArgumentError unless ::File.directory?(target_dir)
 
         @target = target_dir
         @users = load_users
@@ -15,19 +15,19 @@ module SlackScratcher
       def each
         all_files.each do |file|
           chats = parse_log_file(file)
-          yield chats
+          yield chats.refined_data
         end
       end
 
       private
 
       def load_users
-        users = Oj.load(::File.read("#@{target}/users.json"))
+        users = Oj.load(::File.read("#{@target}/users.json"))
         users.map { |data| { data['id'] => data } }.inject({}, :merge)
       end
 
       def parse_log_file(log_file)
-          logs = Oj.load(::File.read(log_file))
+        logs = Oj.load(::File.read(log_file))
         SlackScratcher::Model::Chats.new logs, @users
       end
 
@@ -42,7 +42,7 @@ module SlackScratcher
       end
 
       def list_log_files(channel)
-        Dir["#{@target}/#{channel}/*.json"]
+        Dir["#{channel}*.json"]
       end
     end
   end
