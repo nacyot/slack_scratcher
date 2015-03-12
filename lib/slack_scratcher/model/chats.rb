ff3053c34867869d4cee41ee3adf23a5cca835b7
nacyot <propellerheaven@gmail.com>
2015-03-12 15:00:37 +0900

    Implement model/chat and spec

--- lib/slack_scratcher/model/chats.rb
+++ lib/slack_scratcher/model/chats.rb
@@ -1,30 +1,48 @@
 module SlackScratcher
   module Model
     class Chats
+      attr_reader :refined_data, :data
+
       def initialize(data, users)
+        fail ArgumentError if !data.is_a?(Array) || !users.is_a?(Hash)
+
         @data = data
         @users = users
+        @refined_data = refine
       end
-      
-      def refine
-        @data.map{ |log| refine_data(log) }
-      end
-      
+
       private
+
+      def refine
+        @data.map { |log| refine_data(log) }
+      end
+
       def refine_data(log)
-        user = @users.data[log['user']]
+        user = find_user(log['user']) unless user
+
         log['name'] = user['name']
         log['profile_image'] = user['profile']['image_32']
         log['text'] = refine_text(log['text'])
-        log['datetime'] = Time.at(log['ts']).iso8601
-
+        log['datetime'] = Time.at(log['ts'].to_f)
         log
+      rescue SlackScratcher::Error::UserNotFoundError
+        user = { 'user' => 'undefined' }
+        user['profile'] = { 'image_32' => '' }
       end
-      
+
       def refine_text(text)
-        # TODO - 링크 처리
-        # TODO - 맨션 처리
+        text = ":#{text.to_s}" if text.is_a?(Symbol)
+
         text
+          .gsub(%r{<@([A-Z0-9]{3,10})>}) { '@' + @users[$1]['name'] }
+          .gsub(%r{<(http(s)?://.*?)>}) { $1 }
+      end
+
+      def find_user(user)
+        result = @users[user]
+
+        fail SlackScratcher::Error::UserNotFoundError if result == nil
+        result
       end
     end
   end
--- spec/slack_scratcher/model/chats_spec.rb
+++ spec/slack_scratcher/model/chats_spec.rb
@@ -1,7 +1,79 @@
 require 'spec_helper'
 
 describe SlackScratcher::Model::Chats do
-  it '' do
-    
+  describe 'Initialize object' do
+    it 'requires data and users arguments' do
+      expect do
+        SlackScratcher::Model::Chats.new
+      end.to raise_error(ArgumentError)
+
+      expect do
+        SlackScratcher::Model::Chats.new([], '')
+      end.to raise_error(ArgumentError)
+
+      expect do
+        SlackScratcher::Model::Chats.new('', {})
+      end.to raise_error(ArgumentError)
+
+      expect do
+        SlackScratcher::Model::Chats.new([], {})
+      end.to_not raise_error
+    end
+  end
+
+  describe 'refine method' do
+    let(:datas) do
+      [{ 'user' => 'U03O',
+         'type' => 'message',
+         'text' => "<@U037> <http:\/\/home.org\/index.html> \uac15\uc81c?",
+         'ts' => '1421302869.000002'
+       }]
+    end
+
+    let(:users) do
+      { 'U03O' =>
+        { 'name' => 'nacyot',
+          'profile' => {
+            'image_32' => 'nacyot.png'
+          }
+        },
+        'U037' =>
+        { 'name' => 'toycan' }
+      }
+    end
+
+    let(:chats) { SlackScratcher::Model::Chats.new(datas, users) }
+    let(:log) { chats.refined_data.first }
+    let(:text) { chats.refined_data.first['text'] }
+
+    it 'should returns refined_data' do
+      expect(chats.refined_data).to be_kind_of(Array)
+    end
+
+    it 'should refined_data that is not empty' do
+      expect(chats.refined_data).to_not be_empty
+    end
+
+    it "should have user's information" do
+      expect(log).to include('name' => 'nacyot')
+      expect(log).to include('profile_image' => 'nacyot.png')
+    end
+
+    it "should have message's time" do
+      expect(log['datetime']).to be_kind_of(Time)
+      expect(log['datetime'].iso8601).to eq('2015-01-15T15:21:09+09:00')
+    end
+
+    describe 'refine text' do
+      it 'should have text' do
+        expect(log).to include('text')
+        expect(text).to match(/강제?/)
+      end
+
+      it 'should have a name whiche mentioned' do
+        expect(text).to match(/@toycan/)
+        expect(text).to match(/home.org/)
+      end
+    end
   end
 end
