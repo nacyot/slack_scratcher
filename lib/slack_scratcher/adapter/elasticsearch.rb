b0fa845df28589fa9adbf36ce56bf01c2d2b5ac1
nacyot <propellerheaven@gmail.com>
2015-03-12 14:59:15 +0900

    Implement elasticsearch adapter

--- lib/slack_scratcher/adapter/elasticsearch.rb
+++ lib/slack_scratcher/adapter/elasticsearch.rb
@@ -1,12 +1,33 @@
+require 'elasticsearch'
+
 module SlackScratcher
   module Adapter
     class Elasticsearch
-      def initialize(hosts)
-        @client = Elasticsearch::Client.new hosts: hosts
+      attr_reader :client
+
+      def initialize(hosts, metadata)
+        @client = ::Elasticsearch::Client.new hosts: hosts
+        @metadata = metadata
       end
 
       def send(data)
-        @client.bulk(data)
+        @client.bulk format_bulk(data)
+      end
+
+      private
+
+      def format_bulk(data)
+        { body: data.map { |log| format_log(log) } }
+      end
+
+      def format_log(log)
+        { index:
+             {
+               _index: @metadata[:index],
+               _type: @metadata[:type],
+               data: log
+             }
+         }
       end
     end
   end
