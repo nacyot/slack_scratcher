4a3537c743715e40d7507b837397165d517467a8
nacyot <propellerheaven@gmail.com>
2015-03-15 03:19:16 +0900

    Remove factory_girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'slack_scratcher'
 require 'coveralls'
-require 'factory_girl'
 
 Coveralls.wear!
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :expect }
-  config.include FactoryGirl::Syntax::Methods
 end
