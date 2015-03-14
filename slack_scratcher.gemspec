7aa3d96aa252f636d56b1e31ae16010080acbe8e
nacyot <propellerheaven@gmail.com>
2015-03-15 00:30:22 +0900

    Set-up factory_girl

--- slack_scratcher.gemspec
+++ slack_scratcher.gemspec
@@ -40,6 +40,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency('guard')
   spec.add_development_dependency('guard-rspec')
   spec.add_development_dependency('rspec')
+  spec.add_development_dependency('factory_girl')
 
   # Test Coverage
   spec.add_development_dependency('coveralls')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require 'slack_scratcher'
 require 'coveralls'
 require 'codeclimate-test-reporter'
+require 'factory_girl'
 
 Coveralls.wear!
 CodeClimate::TestReporter.start
+
+RSpec.configure do |config|
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+  config.include FactoryGirl::Syntax::Methods
+end
