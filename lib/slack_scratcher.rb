d7f9fd7745cbd3e20918a8bd03e6f7c943429674
nacyot <propellerheaven@gmail.com>
2015-03-14 15:21:02 +0900

    Add dotenv for development

--- lib/slack_scratcher.rb
+++ lib/slack_scratcher.rb
@@ -1,12 +1,14 @@
 $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'logger'
+require 'dotenv'
+
+Dotenv.load
 
 module SlackScratcher
   autoload :Model, 'slack_scratcher/model'
   autoload :Loader, 'slack_scratcher/loader'
   autoload :Adapter, 'slack_scratcher/adapter'
   autoload :Router, 'slack_scratcher/router'
-  autoload :Collector, 'slack_scratcher/collector'
   autoload :Error, 'slack_scratcher/error'
 
   def self.logger
--- slack_scratcher.gemspec
+++ slack_scratcher.gemspec
@@ -32,6 +32,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency('slack-api', '~> 1')
 
   # Development Dependency
+  spec.add_development_dependency('dotenv')
   spec.add_development_dependency('guard')
   spec.add_development_dependency('guard-rspec')
   spec.add_development_dependency('rspec')
